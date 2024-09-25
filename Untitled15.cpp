63b6712b6582f862700b49618c16c06513c7fed0
Ritam Das <ritamdas2nd787@gmail.com>
2024-09-25 23:37:58 +0530

    Add files via upload

---
+++ Untitled15.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int pass, x=10; 
+    while (x != 0) {
+        printf("\nInput the password (numeric characters only): ");
+        scanf("%d", &pass); 
+        if (pass == 1234) {
+            printf("Correct password"); 
+            x = 0; 
+        } else {
+            printf("Wrong password, try another"); 
+        }
+        printf("\n");
+    }
+    return 0;
+}
