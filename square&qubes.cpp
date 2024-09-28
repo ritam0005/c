6acc9d67e69552816e17b4f1520fb07c7d737c91
Ritam Das <ritamdas2nd787@gmail.com>
2024-09-28 17:13:44 +0530

    Add files via upload

---
+++ square&qubes.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("Number\tSquare\tCube\n");
+    printf("=========================\n");
+    for(x=0; x<=20; x++)
+        printf("%d\t%d\t%d\n", x, x*x, x*x*x);
+    return 0;
+}
+
